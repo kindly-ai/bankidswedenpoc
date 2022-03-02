64d6c9bcb1ef6a352a21dc0aea2d98c17b2b176c
Lars Andre Møen <post@larsandre.no>
2022-03-02 14:39:59 +0100

    ajax post request to backend with ssn from form

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,36 @@
 import express from 'express';
 import * as BankId from 'bankid';
 
+import bodyParser from 'body-parser';
+
+import path from 'node:path';
+
+// Here we are configuring express to use body-parser as middle-ware.
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const client = new BankId.BankIdClient();
 
-const app = express();
 app.get('/auth', (req, res) => {
   client
     .authenticateAndCollect({
-      personalNumber: req.body.pno ?? '196210156342',
+      personalNumber: req.body?.pno ?? '196210156342',
       endUserIp: '127.0.0.1',
     })
     .then((r) => console.log(r.completionData))
     .catch(console.error);
 
-  res.send('auth!');
+  res.send('authenticated');
 });
 
 app.get('/', (request, response) => {
-  response.send('Hello Typescript!');
+  response.sendFile(path.join(`${__dirname}/index.html`));
+});
+
+app.post('/login', (request, response) => {
+  const { ssn } = request.body;
+  response.send('yes');
 });
 
 export default app;
